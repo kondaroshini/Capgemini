b9807add517b633e80c2fa39dc99cdb05d2a32a9
kondaroshini <123068795+kondaroshini@users.noreply.github.com>
2025-05-28 10:09:59 +0530

    Updated

--- Python/assignment5/sample.py
+++ Python/assignment5/sample.py
@@ -1,9 +1,11 @@
+# Writing content to a file
 lines = ['Welcome to file handling, Roshini!!']
 with open('data.txt', 'w') as f:
     for line in lines:
         f.write(line)
         f.write('\n')
 
+# Appending to a file and Reading from a file
 more_lines = ['there are different operations in it','let us discuss one by one','we have read,readline,readlines,write,writelines,append etc.,']
 with open('data.txt', 'a+') as f:
     f.write('\n'.join(more_lines)+'\n')
